958b1bc7b96ef6b6237265dfdb9755a398a753bc
danielverasofi <100155364+danielverasofi@users.noreply.github.com>
2022-02-21 19:58:38 +0200

    Add files via upload

---
+++ Boletin_5/bienvenida.php
@@ -0,0 +1,13 @@
+<html>
+    <head>
+        <title>Bienvenida</title>
+    </head>
+    <body>
+        <h1>
+            <?php
+            echo "Bienvenido, $_POST[nombre]";
+            //muestra texto
+            ?>
+        </h1>
+    </body>
+</html>
---
+++ Boletin_5/nombrecompleto.php
@@ -0,0 +1,12 @@
+<html>
+    <head>
+        <title>ASIR</title>
+    </head>
+    <body>
+        <h1>Te llamas:</h1>
+        <?php
+        echo "$_POST['name'] $_POST['surname']"; //Muestra el valor de las variables asociativas.
+        ?>
+    </body>
+</html>
+
